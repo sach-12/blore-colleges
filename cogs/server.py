3296500ab010410682e4a89efcd12c0f44903b79
ArvindAROO <i.arvindkrishna@gmail.com>
2021-02-01 08:31:49 +0530

    fixed onmemberjoin

--- cogs/server.py
+++ cogs/server.py
@@ -63,8 +63,9 @@ class server(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        self.just_joined = get(self.guildObj.roles, id=805084725710422026)
-        await member.add_roles(self.just_joined)
+        await self.client.get_channel(BOT_LOGS).send(f"{member.mention} joined")
+        just_joined = get(member.guild.roles, id=805084725710422026)
+        await member.add_roles(just_joined)
 
 
     @commands.command(aliases=['gomma', 'ungamman'])
